ba4262e2e88408c214e9604f78dbd0c2d395d5d5
Elton Andrade <eltonsa75@hotmail.com>
2019-06-26 00:58:50 -0300

    Crud MVC- Core

---
+++ Crud/Models/Contexto/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using Crud.Models.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crud.Models.Contexto
+{
+    public class Contexto : DbContext
+    {
+
+        public Contexto(DbContextOptions<Contexto> option) : base(option)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Usuario> Usuario { get; set; }
+    }
+}
